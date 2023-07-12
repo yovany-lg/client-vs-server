b96e04f52f785c3b85fc64e0c908f060aa726348
Yovany Luis <yovany.lg@gmail.com>
2023-07-12 06:36:30 -0600

    demo of server component

---
+++ app/products/page.tsx
@@ -0,0 +1,17 @@
+import { getProducts } from "@/lib/products";
+
+export default async function ProductsPage() {
+  const products = await getProducts();
+  return (
+    <div className="container">
+      <h1 className="text-2xl">Products</h1>
+      {products.map(({ description, id, price, title }) => (
+        <div key={id} className="border p-4 my-4 rounded-md">
+          <h2 className="text-xl">{title}</h2>
+          <p>{description}</p>
+          <p className="text-gray-700">${price}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
